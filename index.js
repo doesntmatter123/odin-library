f0b1f2bd994f0eedcc06c3ace244e3ba9a78c0dd
szczurda <szczuda1@uhk.cz>
2025-01-20 11:28:48 +0100

    some basic stuff

---
+++ index.js
@@ -0,0 +1,17 @@
+
+
+
+const myLibrary = []
+
+
+
+function Book(name, author, pages, hasBeenRead){
+  this.name = name
+  this.author = author
+  this.pages = pages
+  this.hasBeenRead = hasBeenRead
+}
+
+
+
+
